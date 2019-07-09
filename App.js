6d150db51d2c60f07ebbba92c91b483281a6d5ef
Waga <wagaodongo@gmail.com>
2019-07-09 21:49:42 +0300

    Cards

--- App.js
+++ App.js
@@ -3,18 +3,20 @@ import { StyleSheet, Text, View } from 'react-native';
 import Avatar from './components/Avatar';
 import Constants from 'expo-constants';
 import AuthorRow from './components/AuthorRow';
+import Card from './components/Card';
 
 
 export default class App extends React.Component {
   render(){
   return (
     <View style={styles.container}>
-      <AuthorRow 
+      <Card 
       fullname={'First Last'}
       linkText={'Comments'}
       onPressLinkText={()=> {
         console.log('Pressed link!')
-      }}/>
+      }}
+      image={{ uri: 'https://unsplash.it/600/600'}}/>
     </View>
   );
 }
