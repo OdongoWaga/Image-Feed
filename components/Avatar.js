6bf49f1476af8f00fc34bb9f66e72f80de27e979
Waga <wagaodongo@gmail.com>
2019-07-09 15:45:21 +0300

    Avatar 1

---
+++ components/Avatar.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { ColorPropType, View, Text, StyleSheet } from 'react-native';
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+
+export default function Avatar({size, backgroundColor, initials }) {
+
+    <View style={style}
+
+}
+
+Avatar.propTypes ={
+    initials: PropTypes.string.isRequired,
+  size: PropTypes.number.isRequired,
+  backgroundColor: ColorPropType.isRequired,
+}
